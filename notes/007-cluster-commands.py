c8364bbcc7d99034db8a83d32aa9f2efccb78ce1
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-06-13 15:10:15 +0530

    Create 007-cluster-commands.py

---
+++ notes/007-cluster-commands.py
@@ -0,0 +1,27 @@
+open terminal 
+
+port config 
+
+flink-conf.yaml 
+
+
+```
+rest.port = 8282
+```
+
+Start Cluster
+
+```
+
+$FLINK_HOME/bin/start-cluster.sh
+```
+
+
+open browser, http://localhost:8282
+
+
+Submit job
+
+```
+$FLINK_HOME/bin/flink  run -m localhost:8282 -py /home/rps/workshop/notebooks/HelloWorld.py
+```
