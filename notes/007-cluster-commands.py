3ed4252db2ab92a9dcb56533b8a367d1abef1bf0
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-06-13 15:46:25 +0530

    Update 007-cluster-commands.py

--- notes/007-cluster-commands.py
+++ notes/007-cluster-commands.py
@@ -31,3 +31,24 @@ Submit job
 ```
 $FLINK_HOME/bin/flink  run -m localhost:8282 -py /home/rps/workshop/notebooks/HelloWorld.py
 ```
+
+
+
+# checkpoint - managed by flink internallly, on the event of error,
+the checkpoint is used to continue the operations where flink left before crash..
+
+
+
+```
+$FLINK_HOME/bin/flink  run -m localhost:8282 -py /home/rps/workshop/notebooks/HelloWorldStream.py
+
+```
+
+open http://localhost:8282
+    
+ Check in hadoop,
+
+
+http://localhost:50070
+    
+    under browse directories
